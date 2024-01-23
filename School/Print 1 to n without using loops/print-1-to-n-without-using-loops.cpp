54152038d2678cd471fb8c58eb490086b2710413
Deepak babu P <99535087+deepakdeepu000@users.noreply.github.com>
2024-01-23 17:23:52 +0530

    Added Solution - GfG to GitHub

---
+++ School/Print 1 to n without using loops/print-1-to-n-without-using-loops.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    int l = 1;
+    void printTillN(int N)
+    {
+        // Write Your Code here
+        if(l > N) return;
+        cout << l << " ";
+        l+=1;
+        printTillN(N--);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        ob.printTillN(N);
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
