eabc7a9155576ef61445068ff6f971f2db516d61
Deepak babu P <99535087+deepakdeepu000@users.noreply.github.com>
2024-02-11 14:25:50 +0530

    Updated Solution - GfG to GitHub

--- Easy/Recamans sequence/recamans-sequence.cpp
+++ Easy/Recamans sequence/recamans-sequence.cpp
@@ -9,49 +9,51 @@ using namespace std;
 
 class Solution{
 public:
-    vector<int> visited;
-    // vector<int>  helper(int n, vector<int> &v){
-    //     if(n == 0){
-    //         v.push_back(0);
-    //         return v;
-    //     }
-    //     else
-    //     {
-    //         v = helper(n-1, v);
-    //         int ele = v[n-1]-n;
-    //         if( ele > 0 and visited[ele] == 0){
-    //             v.push_back(ele);
-    //             visited[ele] = 1;
-    //         }
-    //         else{
-    //             v.push_back(v[n-1]+n);
-    //             visited[v[n-1]+n] = 1;
-    //         }
-    //     }
-    //     return v;
-    // }
+    bool visited[1000001];
+    vector<int>  helper(int n, vector<int> &v){
+        if(n == 0){
+            v.push_back(0);
+            return v;
+        }
+        else
+        {
+            v = helper(n-1, v);
+            int ele = v[n-1]-n;
+            if( ele > 0 and !visited[ele]){
+                v.push_back(ele);
+                visited[ele] = true;
+            }
+            else{
+                v.push_back(v[n-1]+n);
+                visited[v[n-1]+n] = true;
+            }
+        }
+        return v;
+    }
     
     vector<int> recamanSequence(int n){
         // code here5
         vector<int> v;
-        // visited.resize(1000001, 0);
-        unordered_map<int, int> visited;
-        int i=1;
-        v.push_back(0);
-        while(i<=n)
-        {
-            int ele = v[i-1]-i;
-            if( ele > 0 and visited[ele] == 0){
-                v.push_back(ele);
-                visited[ele]++;
-            }
-            else{
-                v.push_back(v[i-1]+i);
-                visited[v[i-1]+i]++;
-            }
-            i++;
-        }
-        return v;
+        memset(visited,false,sizeof(visited));
+        
+        return helper(n , v);
+        // unordered_map<int, int> visited;
+        // int i=1;
+        // v.push_back(0);
+        // while(i<=n)
+        // {
+        //     int ele = v[i-1]-i;
+        //     if( ele > 0 and visited[ele] == 0){
+        //         v.push_back(ele);
+        //         visited[ele]++;
+        //     }
+        //     else{
+        //         v.push_back(v[i-1]+i);
+        //         visited[v[i-1]+i]++;
+        //     }
+        //     i++;
+        // }
+        // return v;
     }
 };
 
