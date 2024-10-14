c1723cec0f2fdf10bb67b54c6b7013feda8978b0
Deepak babu P <99535087+deepakdeepu000@users.noreply.github.com>
2024-10-14 21:06:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count Linked List Nodes/count-linked-list-nodes.cpp
+++ Difficulty: Basic/Count Linked List Nodes/count-linked-list-nodes.cpp
@@ -1,13 +1,12 @@
 //{ Driver Code Starts
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
-struct Node
-{
+struct Node {
     int data;
-    struct Node* next;
-    
-    Node(int x){
+    struct Node *next;
+
+    Node(int x) {
         data = x;
         next = NULL;
     }
@@ -24,45 +23,42 @@ struct Node
     Node(int x) {  data = x;  next = NULL; }
 }; */
 
-class Solution
-{
-    public:
-    //Function to count nodes of a linked list.
-    int getCount(struct Node* head){
-    
-        //Code here
-        int cnt = 1;
-        Node * temp = head;
-        while(temp->next){
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    int getCount(struct Node* head) {
+
+        // Code here
+        Node* temp = head;
+        int cnt = 0;
+        while(temp){
             cnt++;
-            temp = temp -> next;
+            temp = temp->next;
         }
         return cnt;
-        
     }
 };
-    
 
 
 //{ Driver Code Starts.
 
-int main() 
-{ 
+int main() {
     int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-
-        int data;
-        cin>>data;
-        struct Node *head = new Node(data);
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        struct Node *head = new Node(arr[0]);
         struct Node *tail = head;
-        for (int i = 0; i < n-1; ++i)
-        {
-            cin>>data;
-            tail->next = new Node(data);
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
             tail = tail->next;
         }
         Solution ob;
