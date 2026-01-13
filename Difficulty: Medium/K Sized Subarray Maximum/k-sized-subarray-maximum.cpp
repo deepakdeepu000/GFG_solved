630862a79e244a73ff7a511aab23cecfc362fac9
Deepak babu P <99535087+deepakdeepu000@users.noreply.github.com>
2026-01-13 20:01:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,73 +1,24 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-    // Function to find maximum of each subarray of size k.
-    vector<int> max_of_subarrays(int k, vector<int> &arr) {
-        // your code here
-        deque<int> dq;
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        // code here
+        priority_queue<pair<int , int>> pq;
+        
         vector<int> ans;
         
-        for (int i = 0; i < arr.size(); i++) {
-        // Remove elements from the front of the deque that are out of the window
-        if (!dq.empty() && dq.front() == i - k) {
-            dq.pop_front();
+        for(int i = 0 ;  i < arr.size() ; i++ ){
+            
+            while(!pq.empty() and pq.top().second <= i - k ){
+                pq.pop();
+            }
+            
+            pq.push({arr[i] , i});
+            
+            if( i >= k - 1){
+                ans.push_back(pq.top().first);
+            }
         }
-
-        // Remove elements from the back of the deque that are less than the current element
-        while (!dq.empty() && arr[dq.back()] < arr[i]) {
-            dq.pop_back();
-        }
-
-        // Add the current index to the deque
-        dq.push_back(i);
-
-        // If we've processed at least k elements, add the maximum to the answer
-        if (i >= k - 1) {
-            ans.push_back(arr[dq.front()]); // The maximum is at the front of the deque
-        }
-    }
-
+        
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-
-    while (t--) {
-
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        vector<int> res = obj.max_of_subarrays(k, arr);
-        for (int i = 0; i < res.size(); i++)
-            cout << res[i] << " ";
-        cout << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
