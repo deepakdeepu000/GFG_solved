4c96e2ad18fe55f2296005efcfcaa639124f6aa1
Deepak babu P <99535087+deepakdeepu000@users.noreply.github.com>
2026-02-07 13:42:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Happiest Triplet/happiest-triplet.cpp
+++ Difficulty: Medium/Happiest Triplet/happiest-triplet.cpp
@@ -11,16 +11,26 @@ class Solution {
         int i = 0, j = 0, k = 0;
         while(i < n and j < n and k < n){
             
+            // get the curr min and max of the elements of three arrays.
             int maxi = max({a[i],b[j],c[k]});
             int mini = min({a[i],b[j],c[k]});
+            
+            //calculate the differnece them
             int diff = maxi - mini;
+            
+            // calculate the sum in case the resultant differnce is same we will take the triplets having the least sum
             int sum = a[i] + b[j] + c[k];
             
+            // we will then consider that the triplet is need to be considered or not
             if(prev_diff > diff || ( prev_diff == diff && sum < prev_sum)){
+                
+                // update the values and triplets
                 prev_diff = diff;
                 prev_sum = sum;
                 ans = {a[i],b[j],c[k]};
             }else if(mini == a[i]){
+                // explore the other options.
+                // why we conditioning minimum because we want our minimum value to the as close as possible to our maximum to get minimum difference.
                 i++;
             }else if(mini == b[j]){
                 j++;
@@ -28,6 +38,8 @@ class Solution {
                 k++;
             }
         }
+        
+        //sort the elements in decending order.
         
         sort(ans.begin(), ans.end(), greater<int>());
         return ans;
